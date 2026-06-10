9894d4d10d64cfd791c9e4dc905f16c9cc6aa077
monisrivalli <89726153+monisrivalli@users.noreply.github.com>
2026-06-10 14:57:24 +0530

    Rename file uploader label and update log display

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ import streamlit as st
 st.title("AI Serial Log Analyzer")
 
 uploaded_file = st.file_uploader(
-    "Upload Serial Log",
+    "Upload Log",
     type=["txt", "log"]
 )
 
 if uploaded_file:
+
     log_content = uploaded_file.read().decode("utf-8")
 
-    st.subheader("Uploaded Log")
-
-    st.text(log_content[:2000])
+    st.write(log_content)
